8823c9fd11c91a9d548dc77a7ad80b710c6ad90a
nathanielzacharias <nathanielzacharias@gmail.com>
2022-09-03 16:15:14 +0800

    feat: added db connection to mongo Atlas, server works and listens on 8000

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+require('dotenv').config()
+
+const connectToMongo = require('./database/mongodb')
+
+const mongoose = require('mongoose')
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//Server
+app.listen(port, async () => {
+
+    try {
+        await mongoose.connect(connectToMongo.uri, { useNewUrlParser: true, useUnifiedTopology: true } )
+
+    } catch (err) {
+        console.log('Failed to connect to Mongo Atlas')
+        process.exit(1)
+    }
+
+    console.log(`Rent-Go-Where is listening on port ${port}`);
+
 })
