1832fd20f9a970970041cff1930138d91a4c6563
Kendrick Winata <kendrickwinata@Najwas-MacBook-Pro-2.local>
2022-09-08 01:11:38 +0800

    chore: install and use cors

--- routers/routes.js
+++ routers/routes.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const controller = require('../controllers/controller')
+const controller = require("../controllers/controller");
 
 //Routes from /api/v1
-router.get('/', controller.homepage)
-router.get('/app/show_properties', controller.indexProperties)
-router.get('/app/show_potentialHousemates', controller.indexPotentialHousemates)
+router.get("/", controller.homepage);
+router.get("/app/show_properties", controller.indexProperties);
+router.get(
+  "/app/show_potentialHousemates",
+  controller.indexPotentialHousemates
+);
 
-module.exports = router 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const connectToMongo = require('./database/mongodb')
+const connectToMongo = require("./database/mongodb");
 
-const mongoose = require('mongoose')
-const express = require('express')
-const req = require('express/lib/request')
-const app = express()
-const port = process.env.PORT || 8000
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const req = require("express/lib/request");
+const app = express();
+const port = process.env.PORT || 8000;
 
-const apiRouter = require('./routers/routes')
+const apiRouter = require("./routers/routes");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //Router
-app.use('/api/v1', apiRouter)
+app.use("/api/v1", apiRouter);
 
 //Server
 app.listen(port, async () => {
+  try {
+    await mongoose.connect(connectToMongo.uri, {
+      dbName: process.env.DB_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.log("Failed to connect to Mongo Atlas. Error is: ", err);
+    process.exit(1);
+  }
 
-    try {
-        await mongoose.connect(connectToMongo.uri, { dbName: process.env.DB_NAME, useNewUrlParser: true, useUnifiedTopology: true } )
-
-    } catch (err) {
-        console.log('Failed to connect to Mongo Atlas. Error is: ', err)
-        process.exit(1)
-    }
-
-    console.log(`Rent-Go-Where is listening on port ${port}`);
-
-})
+  console.log(`Rent-Go-Where is listening on port ${port}`);
+});
