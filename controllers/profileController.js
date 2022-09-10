a98b28f753e4e74cd271ab2a0ffc4485e6d2b0f8
Yui Xiong Ng <jadebear15@gmail.com>
2022-09-10 23:41:19 +0800

    feat:delete route for User

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -7,20 +7,19 @@ module.exports = {
   updateUser: async (req, res) => {
     // do validation...
     //if it fails when merging with react. its likely due to the need to decode jwt and get objectID out
-   let user = null
+    
+    let user = null;
     try {
-        // genderToUpadte = req.body.gender
-        user = await userModel.findByIdAndUpdate(
-          req.body.userId,
-          {
-            gender : req.body.gender,
-            followedUsers: req.body.followedUsers,
-            followedProperties: req.body.followedProperties,
-            nationality: req.body.nationality
-          }
-        );
+
+      user = await userModel.findByIdAndUpdate(req.body.userId, {
+        gender: req.body.gender,
+        followedUsers: req.body.followedUsers,
+        followedProperties: req.body.followedProperties,
+        nationality: req.body.nationality,
+      });
 
     } catch (err) {
+
       res.status(500);
       return res.json({ error: `Fail to get user of id ${req.body.userId}` });
     }
@@ -32,16 +31,42 @@ module.exports = {
     return res.json({});
   },
 
-  
+  deleteUser: async (req, res) => {
+    // do validation...
+
+    const userId = req.body.userId;
+    let user = null;
+
+    try {
+      user = await userModel.findById(userId);
+    } catch (err) {
+      res.status(500);
+      return res.json({ error: `Fail to get user of id ${userId}` });
+    }
+
+    if (!user) {
+      res.status(404);
+      return res.json(user);
+    }
+
+    try {
+      await user.delete();
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "failed to delete user" });
+    }
+
+    return res.json();
+  },
 };
 //casting objectID string into objectid and pass into mongodb
-   // try {
-    //   user = await userModel.findByIdAndUpdate(
-    //     req.body.userId,
-    //     {
-    //       $push: {
-    //         nationality: mongoose.Types.ObjectId(req.body.nationality),
-    //       },
-    //     },
-    //     { new: true }
-    //   );
+// try {
+//   user = await userModel.findByIdAndUpdate(
+//     req.body.userId,
+//     {
+//       $push: {
+//         nationality: mongoose.Types.ObjectId(req.body.nationality),
+//       },
+//     },
+//     { new: true }
+//   );
--- routers/profileRoutes.js
+++ routers/profileRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const profileController = require("../controllers/profileController");
 
 router.patch('/update', profileController.updateUser)
+router.delete('/delete', profileController.deleteUser)
 
 module.exports = router;
