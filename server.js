818e24b9db319455bb96fa84f45c223ccd45c0b2
Yui Xiong Ng <jadebear15@gmail.com>
2022-09-10 22:48:30 +0800

    chore: added objectID when user login encoded in JWT token

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const req = require("express/lib/request");
 const app = express();
 const port = process.env.PORT || 8000;
 
+//routes
 const apiRouter = require("./routers/routes");
 const userRouter = require("./routers/userRoutes")
+const profileRouter = require("./routers/profileRoutes")
 
 // app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -22,6 +24,7 @@ app.use(
 
 //Router
 app.use("/api/v1/auth",userRouter);
+app.use("/api/v1/profile",profileRouter)
 app.use("/api/v1", apiRouter);
 
 app.get("/", (req,res) => {
