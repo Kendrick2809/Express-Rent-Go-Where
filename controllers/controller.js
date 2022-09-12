2cd951c9d1a3e5ed13fd5fc81552513dc24cd049
Yui Xiong Ng <jadebear15@gmail.com>
2022-09-13 00:44:12 +0800

    feat: create property route done. tested with postman

--- controllers/controller.js
+++ controllers/controller.js
@@ -41,4 +41,29 @@ module.exports = {
     return res.json(potentialHosemates);
   },
 
+  indexSingleProperties: async (req, res) => {
+    let property = [];
+
+    try {
+      property = await homeModel.findById(req.params.propID);
+      // console.log(properties)
+    } catch (err) {
+      res.status(500);
+      return res.json({ error: "failed to return indexProperties" });
+    }
+
+    return res.json(property);
+  },
+  
+  createProperties: async (req, res) => {
+
+    try {
+      await homeModel.create(req.body);
+    } catch (err) {
+      res.status(500);
+      return res.json({ error: "failed to create a single property" });
+    }
+    return res.status(201).json()
+  },
+
 };
--- routers/routes.js
+++ routers/routes.js
@@ -7,9 +7,8 @@ const controller = require("../controllers/controller");
 //Routes from /api/v1
 router.get("/", controller.homepage);
 router.get("/app/show_properties", controller.indexProperties);
-router.get(
-  "/app/show_potentialHousemates",
-  controller.indexPotentialHousemates
-);
+router.get("/app/show_potentialHousemates", controller.indexPotentialHousemates);
+router.get("/app/show_properties/:propID", controller.indexSingleProperties)
+router.post("/app/create_properties", controller.createProperties);
 
 module.exports = router;
